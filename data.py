ae974a5606f88c00f3ef1e894fad10e1275cdb46
Klondike <147417684+MyKlondike@users.noreply.github.com>
2023-10-14 16:32:43 +0500

    Create data.py
    add data

---
+++ data.py
@@ -0,0 +1,5 @@
+DATA = {
+    'ethereum'      : {'rpc': 'https://rpc.ankr.com/eth', 'scan': 'https://etherscan.io/tx', 'token': 'ETH', 'chain_id': 1},
+
+    'base'          : {'rpc': 'https://mainnet.base.org', 'scan': 'https://basescan.org/tx', 'token': 'ETH', 'chain_id': 8453},
+}
